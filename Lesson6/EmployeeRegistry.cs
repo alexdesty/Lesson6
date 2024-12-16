fb23c5e949661c90657ebd47ecefe917b3d05b4d
alexdesty <alex.desty@gmail.com>
2024-12-16 08:28:25 +0300

    Add some code

---
+++ Lesson6/EmployeeRegistry.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson6;
+public static class EmployeeRegistry
+{
+    public static void AddEmployee(EmployeeBase employee)
+    {
+
+    }
+
+    public static void ListAllEmployees()
+    {
+
+    }
+
+    public static void FindEmployee(string name)
+    {
+
+    }
+}
+
---
+++ Lesson6/Manager.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson6;
+internal class Manager : EmployeeBase
+{
+    public override string Name { get; set; }
+    public override string Position { get; set; }
+
+    public string Department { get; set; }
+
+    public override void GetDetails()
+    {
+        Console.WriteLine($"List of managers\nManager's name: {Name}\nManager's position: {Position}\nManager's department:{Department}\n");
+    }
+}
+
